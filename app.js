ac07864cdc9a87c7cb33e6095392eaa3f295d31b
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-12-01 14:03:15 +0200

    started

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('build'));
+
+function logger(req, res, next) {
+	console.log(
+		`request fired to: '${req.url}', Method: '${req.method}', at: ${
+			new Date().toString().split('GMT')[0]
+		}`
+	);
+	next();
+}
+app.use(logger);
+
+// get endpoint
+app.get('/api/posts', async (req, res, next) => {
+	// await scrapeData();
+	let data = await readFile('data.json', 'utf8');
+	data = JSON.parse(data);
+	res.json({ data });
+	// don't forget: catch((error) => next(error));
+});
+
+/// ERRORS SECTION
+
+const unknownEndpointHandler = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpointHandler);
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	// build here your error handler
+
+	next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
+
+module.exports = app;
